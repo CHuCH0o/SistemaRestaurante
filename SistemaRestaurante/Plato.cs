ac7b602f878f28cdfb4fbaa1a0cbc9c1ed81c295
royer123321 <rcamilocubillos1217@gmail.com>
2025-11-12 15:13:30 -0500

    se inicializa la case plato

--- SistemaRestaurante/Plato.cs
+++ SistemaRestaurante/Plato.cs
@@ -1,6 +1,12 @@
-namespace SistemaRestaurante;
-
-public class Plato
+namespace SistemaRestaurante
 {
-    
+    // esta clase guarda los datos del plato
+
+    public class Plato
+    {
+        public string Codigo;
+        public string Nombre;
+        public string Descripcion;
+        public decimal Precio;
+    }
 }
