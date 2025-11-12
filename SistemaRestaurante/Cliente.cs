82194683d465da25327a1cbcbf1a24de2d0b5ce2
royer123321 <rcamilocubillos1217@gmail.com>
2025-11-12 15:10:43 -0500

    se iniciliza la clase cliente

--- SistemaRestaurante/Cliente.cs
+++ SistemaRestaurante/Cliente.cs
@@ -1,6 +1,12 @@
-namespace SistemaRestaurante;
-
-public class Cliente
+namespace SistemaRestaurante
 {
-    
+    // esta clase guarda los datos del cliente
+
+    public class Cliente
+    {
+        public string Cedula;
+        public string NombreCompleto;
+        public string Celular;
+        public string Email;
+    }
 }
