234d1f79b6000b6821da1e4d51cefab8a2c1ea71
CHuCH0o <jjgutierrez113077@umanizales.edu.co>
2025-11-18 20:30:16 -0500

    se actualiza la clase Restaurante.cs

--- SistemaRestaurante/Restaurante.cs
+++ SistemaRestaurante/Restaurante.cs
@@ -1,6 +1,5 @@
 using System;
 
-
 namespace SistemaRestaurante
 {
     // esta clase guarda los datos del restaurante
@@ -12,5 +11,22 @@ namespace SistemaRestaurante
         public string Dueno;
         public string Celular;
         public string Direccion;
+        
+        // estas son las listas relacionadas con el restaurante
+        public ListaEnlazada<Cliente> Clientes;
+        public ListaEnlazada<Plato> Platos;
+        public Cola<Pedido> ColaPedidos;
+        public Pila<Pedido> HistorialPedidos;
+        public decimal GananciasDia;
+
+        // constructor para dejar todo listo
+        public Restaurante()
+        {
+            Clientes = new ListaEnlazada<Cliente>();
+            Platos = new ListaEnlazada<Plato>();
+            ColaPedidos = new Cola<Pedido>();
+            HistorialPedidos = new Pila<Pedido>();
+            GananciasDia = 0;
+        }
     }
 }
