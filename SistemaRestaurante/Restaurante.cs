5bead02b7c4e9b70f023102c0822afbee479e221
CHuCH0o <jjgutierrez113077@umanizales.edu.co>
2025-11-18 20:53:19 -0500

    se agrega la busqueda por celular y nit

--- SistemaRestaurante/Restaurante.cs
+++ SistemaRestaurante/Restaurante.cs
@@ -27,6 +27,46 @@ namespace SistemaRestaurante
             ColaPedidos = new Cola<Pedido>();
             HistorialPedidos = new Pila<Pedido>();
             GananciasDia = 0;
+            
+        }
+
+        // aqui reviso que el celular tenga diez numeros
+        public static bool EsCelularValido(string celular)
+        {
+            if (string.IsNullOrWhiteSpace(celular))
+            {
+                return false;
+            }
+
+            if (celular.Length != 10)
+            {
+                return false;
+            }
+
+            foreach (char c in celular)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // aqui busco un restaurante por nit en la lista
+        public static Restaurante BuscarPorNit(ListaEnlazada<Restaurante> restaurantes, string nit)
+        {
+            Nodo<Restaurante> actual = restaurantes.Cabeza;
+            while (actual != null)
+            {
+                if (actual.Valor.Nit == nit)
+                {
+                    return actual.Valor;
+                }
+                actual = actual.Siguiente;
+            }
+            return null;
         }
     }
 }
